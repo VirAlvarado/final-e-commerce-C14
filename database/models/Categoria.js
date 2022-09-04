c893408b4f13de06dcd7185bef6984a0d7dafbcd
VirAlvarado <viralvarado.994@gmail.com>
2022-09-04 18:02:27 -0300

    e-commerce

---
+++ database/models/Categoria.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nombre: {
+            type: DataTypes.STRING
+        }
+    };
+
+    let config = {
+        tableName: "categorias",
+        timestamps: false
+    };
+
+    const Categoria = sequelize.define("Categoria", cols, config);
+
+    Categoria.associate = function (models) {
+        Categoria.hasMany(models.Producto, { 
+            foreignKey: 'categoria_id',
+            as: 'producto'
+        })
+    }
+
+    return Categoria;
+
+}
